f5924fcf62e30dc7b278ef56bd4fbe10f8281beb
Lukáš Sobotka <115837887+glofin@users.noreply.github.com>
2023-10-19 15:58:13 +0200

    Základní logika 1.1

--- C/uno.h
+++ C/uno.h
@@ -10,18 +10,17 @@
 #define blue 3
 #define green 4
 
-typedef struct _card
-{
+typedef struct _card {
     int colour;
     int number;
 } card;
 
 
-typedef struct _player
-{
+typedef struct _player {
+    char  nickname[15];
+    char ip[20];
+    int stage; /*-1 -> odpojen, 1 -> pripojen beze jmena, 2 -> pojmenovany v lobby, 3 -> pripojen k roomce, 4 -> ve hre*/
     card_list_node * card_list;
 } player;
-
-
 
 #endif
